ea2bb8f8a710324da43f7d731ab66b65ce846a6a
PJ Cribbin <CribbinPJ@gmail.com>
2016-03-27 13:25:59 +0100

    Set up history activity

--- app/src/main/java/com/pjcribbin/healthtracker/History.java
+++ app/src/main/java/com/pjcribbin/healthtracker/History.java
@@ -2,17 +2,12 @@ package com.pjcribbin.healthtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class Home extends AppCompatActivity {
+public class History extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
-
-    public void displayHistory(View view) {
         setContentView(R.layout.activity_history);
     }
 }
