36a9c959f9c5071c8832b119d324bf9593ffa4a9
PJ Cribbin <CribbinPJ@gmail.com>
2016-03-27 13:28:15 +0100

    Set up stats activity

--- app/src/main/java/com/pjcribbin/healthtracker/Stats.java
+++ app/src/main/java/com/pjcribbin/healthtracker/Stats.java
@@ -2,21 +2,12 @@ package com.pjcribbin.healthtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class Home extends AppCompatActivity {
+public class Stats extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
-
-    public void displayHistory(View view) {
-        setContentView(R.layout.activity_history);
-    }
-
-    public void displayStats(View view) {
         setContentView(R.layout.activity_stats);
     }
 }
