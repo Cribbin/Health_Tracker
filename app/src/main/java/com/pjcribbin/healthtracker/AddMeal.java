a7943dd03bac56b899dd4cc865ff8a34dc3e36f6
PJ Cribbin <CribbinPJ@gmail.com>
2016-04-23 20:08:42 +0100

    Add options menu to AddMeal, History and Stats activities

--- app/src/main/java/com/pjcribbin/healthtracker/AddMeal.java
+++ app/src/main/java/com/pjcribbin/healthtracker/AddMeal.java
@@ -3,6 +3,7 @@ package com.pjcribbin.healthtracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -25,5 +26,11 @@ public class AddMeal extends AppCompatActivity {
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, food);
 
         foodList.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
--- app/src/main/java/com/pjcribbin/healthtracker/Stats.java
+++ app/src/main/java/com/pjcribbin/healthtracker/Stats.java
@@ -2,6 +2,7 @@ package com.pjcribbin.healthtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Stats extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class Stats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
