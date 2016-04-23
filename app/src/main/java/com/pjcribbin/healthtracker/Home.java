12702251b959eaddb5023b5c5dc7182515812321
PJ Cribbin <CribbinPJ@gmail.com>
2016-04-23 17:54:33 +0100

    Add app exit confirm alert

--- app/src/main/java/com/pjcribbin/healthtracker/Home.java
+++ app/src/main/java/com/pjcribbin/healthtracker/Home.java
@@ -1,7 +1,13 @@
 package com.pjcribbin.healthtracker;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 
 public class Home extends AppCompatActivity {
@@ -10,6 +16,28 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_lock_power_off)
+                .setTitle("Do you wish to exit?")
+                .setMessage("Do you want to exit the app?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 
     public void displayHistory(View view) {
