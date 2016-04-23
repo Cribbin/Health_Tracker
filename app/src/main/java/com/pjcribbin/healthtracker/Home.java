1b4e6b6dc6f9c28ce3fe2e135a9cef7799ae6506
PJ Cribbin <CribbinPJ@gmail.com>
2016-04-23 19:22:14 +0100

    Add Add Meal activity

--- app/src/main/java/com/pjcribbin/healthtracker/Home.java
+++ app/src/main/java/com/pjcribbin/healthtracker/Home.java
@@ -1,12 +1,10 @@
 package com.pjcribbin.healthtracker;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
@@ -26,25 +24,51 @@ public class Home extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        new AlertDialog.Builder(this)
-                .setIcon(android.R.drawable.ic_lock_power_off)
-                .setTitle("Do you wish to exit?")
-                .setMessage("Do you want to exit the app?")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                })
-                .setNegativeButton("No", null)
-                .show();
+        if (isTaskRoot()) {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_lock_power_off)
+                    .setTitle("Do you wish to exit?")
+                    .setMessage("Do you want to exit the app?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        }
     }
 
     public void displayHistory(View view) {
         setContentView(R.layout.activity_history);
     }
 
+    public void displayAddMeal(View view) {
+        setContentView(R.layout.activity_add_meal);
+    }
+
     public void displayStats(View view) {
         setContentView(R.layout.activity_stats);
+    }
+
+    protected void createDatabase() {
+        try {
+            SQLiteDatabase database = this.openOrCreateDatabase("Fitness", MODE_PRIVATE, null);
+
+            database.execSQL("CREATE TABLE IF NOT EXISTS Food" +
+                    "(food_id INT PRIMARY KEY," +
+                    "food_name VARCHAR(32)," +
+                    "calories INT," +
+                    "fat DOUBLE," +
+                    "protein DOUBLE," +
+                    "sugar DOUBLE," +
+                    "salt DOUBLE," +
+                    "fiber DOUBLE)");
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
