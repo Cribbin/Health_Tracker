d8a42d4de0603d2f174c335fb041128250d2c706
PJ Cribbin <CribbinPJ@gmail.com>
2016-04-25 21:04:36 +0100

    Add Meal and Food_Meal table to DB

--- app/src/main/java/com/pjcribbin/healthtracker/Database.java
+++ app/src/main/java/com/pjcribbin/healthtracker/Database.java
@@ -1,12 +1,13 @@
 package com.pjcribbin.healthtracker;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class Database extends SQLiteOpenHelper {
-    private static int version = 3;
+    private static int version = 4;
     private final static String TAG = "PJ_Health_Tracker";
 
     public Database(Context ctx) {
@@ -28,6 +29,15 @@ public class Database extends SQLiteOpenHelper {
                     "sodium REAL," +
                     "sugar REAL)");
 
+            db.execSQL("CREATE TABLE IF NOT EXISTS Meal" +
+                    "(_id INTEGER PRIMARY KEY)");
+
+            db.execSQL("CREATE TABLE IF NOT EXISTS Food_Meal" +
+                    "(food_id INTEGER," +
+                    "meal_id INTEGER," +
+                    "PRIMARY KEY (food_id, meal_id)" +
+                    ")");
+
             Log.i(TAG, "Database created successfully");
 
         } catch (Exception e) {
@@ -38,18 +48,36 @@ public class Database extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer) {
-        db.execSQL("DROP TABLE Food");
+        try {
+            db.execSQL("DROP TABLE IF EXISTS Food");
 
-        db.execSQL("CREATE TABLE IF NOT EXISTS Food" +
-                "(_id INTEGER PRIMARY KEY," +
-                "food_name TEXT," +
-                "calories INTEGER," +
-                "carbohydrates REAL," +
-                "fat REAL," +
-                "protein REAL," +
-                "sodium REAL," +
-                "sugar REAL)");
+            db.execSQL("CREATE TABLE IF NOT EXISTS Food" +
+                    "(_id INTEGER PRIMARY KEY," +
+                    "food_name TEXT," +
+                    "calories INTEGER," +
+                    "carbohydrates REAL," +
+                    "fat REAL," +
+                    "protein REAL," +
+                    "sodium REAL," +
+                    "sugar REAL)");
 
-        Log.i(TAG, "Database updated to Version " + newVer);
+            db.execSQL("DROP TABLE IF EXISTS Meal");
+
+            db.execSQL("CREATE TABLE IF NOT EXISTS Meal" +
+                    "(_id INTEGER PRIMARY KEY)");
+
+            db.execSQL("DROP TABLE IF EXISTS Food_Meal");
+
+            db.execSQL("CREATE TABLE IF NOT EXISTS Food_Meal" +
+                    "(food_id INTEGER," +
+                    "meal_id INTEGER," +
+                    "PRIMARY KEY (food_id, meal_id)" +
+                    ")");
+
+            Log.i(TAG, "Database updated from version " + oldVer + " to version " + newVer);
+        } catch (Exception e) {
+            Log.e(TAG, "Database failed to update from version " + oldVer + " to version " + newVer);
+            e.printStackTrace();
+        }
     }
 }
