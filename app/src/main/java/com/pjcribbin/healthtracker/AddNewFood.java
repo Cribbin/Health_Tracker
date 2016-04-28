2dd29d3bab6b5934daf55195f67713176e0e302e
PJ Cribbin <CribbinPJ@gmail.com>
2016-04-28 10:35:22 +0100

    Add check if food name is already in database

--- app/src/main/java/com/pjcribbin/healthtracker/AddNewFood.java
+++ app/src/main/java/com/pjcribbin/healthtracker/AddNewFood.java
@@ -1,6 +1,7 @@
 package com.pjcribbin.healthtracker;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
 import android.support.v7.app.AppCompatActivity;
@@ -52,65 +53,82 @@ public class AddNewFood extends AppCompatActivity {
 
         Database dbHelper = new Database(this);
         db = dbHelper.getWritableDatabase();
+        Cursor c;
 
         String query = "INSERT INTO Food (food_name, calories, carbohydrates, fat, protein, sodium, sugar) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        String checkQuery = "SELECT _id, food_name FROM Food Where food_name = ";
+
+        int nameInDb = -1;
         SQLiteStatement statement = db.compileStatement(query);
 
 
         if (foodName.matches("") || foodName.matches("\\s+")) {
             Toast.makeText(getApplicationContext(), "Food name required", Toast.LENGTH_SHORT).show();
         } else {
-            statement.bindString(1, foodName);
-
-            if (!calories.matches("")) {
-                statement.bindString(2, calories);
-            } else {
-                statement.bindNull(2);
-            }
-
-            if (!carbohydrates.matches("")) {
-                statement.bindString(3, carbohydrates);
-            } else {
-                statement.bindNull(3);
-            }
-
-            if (!fat.matches("")) {
-                statement.bindString(4, fat);
-            } else {
-                statement.bindNull(4);
-            }
-
-            if (!protein.matches("")) {
-                statement.bindString(5, protein);
-            } else {
-                statement.bindNull(5);
-            }
-
-            if (!sodium.matches("")) {
-                statement.bindString(6, sodium);
-            } else {
-                statement.bindNull(6);
-            }
-
-            if (!potassium.matches("")) {
-                statement.bindString(7, potassium);
-            } else {
-                statement.bindNull(7);
-            }
-
+            checkQuery += "'" + foodName + "'";
             try {
-                statement.execute();
-            } catch (Exception e) {
-                Toast.makeText(AddNewFood.this, "Error creating " + foodName, Toast.LENGTH_SHORT).show();
-                Log.e(TAG, "INSERT INTO Food Error:\n" + query);
+                c = db.rawQuery(checkQuery, null);
+                nameInDb = c.getCount();
+            } catch(Exception e) {
                 e.printStackTrace();
-            } finally {
-                db.close();
+                Log.e(TAG, "Error checking database\nQuery: " + checkQuery);
             }
 
-            Toast.makeText(AddNewFood.this, foodName + " created successfully", Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(getApplicationContext(), AddFood.class);
-            startActivity(i);
+            if (nameInDb > 0) {
+                Toast.makeText(getApplicationContext(), "Food name" + foodName + " already exists", Toast.LENGTH_SHORT).show();
+            } else {
+                statement.bindString(1, foodName);
+
+                if (!calories.matches("")) {
+                    statement.bindString(2, calories);
+                } else {
+                    statement.bindNull(2);
+                }
+
+                if (!carbohydrates.matches("")) {
+                    statement.bindString(3, carbohydrates);
+                } else {
+                    statement.bindNull(3);
+                }
+
+                if (!fat.matches("")) {
+                    statement.bindString(4, fat);
+                } else {
+                    statement.bindNull(4);
+                }
+
+                if (!protein.matches("")) {
+                    statement.bindString(5, protein);
+                } else {
+                    statement.bindNull(5);
+                }
+
+                if (!sodium.matches("")) {
+                    statement.bindString(6, sodium);
+                } else {
+                    statement.bindNull(6);
+                }
+
+                if (!potassium.matches("")) {
+                    statement.bindString(7, potassium);
+                } else {
+                    statement.bindNull(7);
+                }
+
+                try {
+                        statement.execute();
+                } catch (Exception e) {
+                    Toast.makeText(AddNewFood.this, "Error creating " + foodName, Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "INSERT INTO Food Error:\n" + query);
+                    e.printStackTrace();
+                } finally {
+                    db.close();
+                }
+
+                Toast.makeText(AddNewFood.this, foodName + " created successfully", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getApplicationContext(), AddFood.class);
+                startActivity(i);
+            }
         }
     }
 }
