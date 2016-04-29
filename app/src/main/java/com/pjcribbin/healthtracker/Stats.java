275ccdcd23af65ee2628089399cd1205e056775c
PJ Cribbin <CribbinPJ@gmail.com>
2016-04-29 15:42:27 +0100

    Set up stats with external library

--- app/src/main/java/com/pjcribbin/healthtracker/Stats.java
+++ app/src/main/java/com/pjcribbin/healthtracker/Stats.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.github.mikephil.charting.charts.LineChart;
+
 public class Stats extends AppCompatActivity {
     private final static String TAG = "PJ_Health_Tracker";
 
@@ -11,6 +13,8 @@ public class Stats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+
+        LineChart chart = (LineChart) findViewById(R.id.chart);
     }
 
     @Override
