7435096c3d3ccac5159715a87b31207536d89c4c
PJ Cribbin <CribbinPJ@gmail.com>
2016-04-29 11:24:07 +0100

    Streamline database helper

--- app/src/main/java/com/pjcribbin/healthtracker/Database.java
+++ app/src/main/java/com/pjcribbin/healthtracker/Database.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class Database extends SQLiteOpenHelper {
-    private static int version = 29;
+    private static int version = 30;
     private final static String TAG = "PJ_Health_Tracker";
 
     public Database(Context ctx) {
@@ -53,7 +53,7 @@ public class Database extends SQLiteOpenHelper {
             Log.i(TAG, "Database created successfully");
 
         } catch (Exception e) {
-            Log.e(TAG, "Could not create Food table");
+            Log.e(TAG, "Could not create Database");
             e.printStackTrace();
         }
     }
@@ -62,46 +62,12 @@ public class Database extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer) {
         try {
             db.execSQL("DROP TABLE IF EXISTS Food");
-
-            db.execSQL("CREATE TABLE IF NOT EXISTS Food" +
-                    "(_id INTEGER PRIMARY KEY," +
-                    "food_name TEXT UNIQUE," +
-                    "calories INTEGER," +
-                    "carbohydrates REAL," +
-                    "fat REAL," +
-                    "protein REAL," +
-                    "sodium REAL," +
-                    "sugar REAL)");
-
             db.execSQL("DROP TABLE IF EXISTS Meal");
-
-            db.execSQL("CREATE TABLE IF NOT EXISTS Meal" +
-                    "(_id INTEGER PRIMARY KEY," +
-                    "meal_type TEXT," +
-                    "meal_name TEXT UNIQUE)");
-
             db.execSQL("DROP TABLE IF EXISTS Food_Meal");
-
-            db.execSQL("CREATE TABLE IF NOT EXISTS Food_Meal" +
-                    "(food_id INTEGER," +
-                    "meal_id INTEGER," +
-                    "qty INTEGER," +
-                    "PRIMARY KEY (food_id, meal_id)" +
-                    ")");
-
             db.execSQL("DROP TABLE IF EXISTS Meal_Entry");
-
-            db.execSQL("CREATE TABLE IF NOT EXISTS Meal_Entry" +
-                    "(meal_id INTEGER," +
-                    "timestamp DATETIME DEFAULT (datetime('now', 'localtime'))," +
-                    "PRIMARY KEY (meal_id, timestamp)" +
-                    ")");
-
             db.execSQL("DROP TABLE IF EXISTS Num_Steps");
 
-            db.execSQL("CREATE TABLE IF NOT EXISTS Num_Steps" +
-                    "(day DATETIME DEFAULT (date('now', 'localtime')) PRIMARY KEY," +
-                    "steps INTEGER DEFAULT 0)");
+            onCreate(db);
 
             Log.i(TAG, "Database updated from version " + oldVer + " to version " + newVer);
         } catch (Exception e) {
