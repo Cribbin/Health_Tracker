854fc6c0624cb5c0b1316e5e73fbb0ac5062766f
PJ Cribbin <CribbinPJ@gmail.com>
2016-04-30 12:27:51 +0100

    Tidy error handling

--- app/src/main/java/com/pjcribbin/healthtracker/DateReceiver.java
+++ app/src/main/java/com/pjcribbin/healthtracker/DateReceiver.java
@@ -3,12 +3,9 @@ package com.pjcribbin.healthtracker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
-import android.os.Message;
 import android.util.Log;
-import android.widget.Toast;
 
 public class DateReceiver extends BroadcastReceiver {
     private final static String TAG = "PJ_Health_Tracker";
@@ -20,6 +17,7 @@ public class DateReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context c, Intent intent) {
         this.context = c;
+        Log.i(TAG, "Date message received");
         Thread thread = new Thread(r);
         thread.start();
     }
@@ -34,8 +32,7 @@ public class DateReceiver extends BroadcastReceiver {
                 db = dbHelper.getWritableDatabase();
                 db.execSQL("PRAGMA foreign_keys = ON");
             } catch (Exception e) {
-                Log.e(TAG, "Error opening database");
-                e.printStackTrace();
+                Log.e(TAG, "Error opening database\nStack Trace:\n" + Log.getStackTraceString(e));
             }
 
             try {
@@ -45,8 +42,7 @@ public class DateReceiver extends BroadcastReceiver {
                 statement.execute();
                 Log.i(TAG, "Today's date added");
             } catch (Exception e) {
-                e.printStackTrace();
-                Log.w(TAG, "Day already exists");
+                Log.w(TAG, "Day already exists\nStack Trace:\n" + Log.getStackTraceString(e));
             }
 
             Log.i(TAG, "Add new date tread complete");
