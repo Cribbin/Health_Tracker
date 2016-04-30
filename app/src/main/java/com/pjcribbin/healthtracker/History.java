3e7d1a75ab5e47c6deffd1487bca81267f18802b
PJ Cribbin <CribbinPJ@gmail.com>
2016-04-30 16:32:11 +0100

    Add context menu to history list view items

--- app/src/main/java/com/pjcribbin/healthtracker/History.java
+++ app/src/main/java/com/pjcribbin/healthtracker/History.java
@@ -5,13 +5,21 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class History extends AppCompatActivity {
     private final static String TAG = "PJ_Health_Tracker";
     private static SQLiteDatabase db;
+    private String clickedMealName;
+    private String clickedMealId;
+    private String clickedMealDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,7 @@ public class History extends AppCompatActivity {
 
     private void populateListViewWithMealEntries() {
         ListView mealList = (ListView) findViewById(R.id.meal_history_list);
+        registerForContextMenu(mealList);
         Cursor c;
 
         try {
@@ -36,6 +45,21 @@ public class History extends AppCompatActivity {
         } catch (Exception e) {
             Log.e(TAG, "Error setting up list from database\nStack Trace:\n" + Log.getStackTraceString(e));
             Toast.makeText(getApplicationContext(), "Error setting up list", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+        clickedMealName = ((TextView) info.targetView.findViewById(R.id.meal_name)).getText().toString();
+        clickedMealDate = ((TextView) info.targetView.findViewById(R.id.meal_time)).getText().toString();
+        clickedMealId = ((TextView) info.targetView.findViewById(R.id.meal_id)).getText().toString();
+        menu.setHeaderTitle(clickedMealName + "\n(" + clickedMealDate + ")");
+
+        if (v.getId()==R.id.meal_history_list) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu_history_options, menu);
         }
     }
 
